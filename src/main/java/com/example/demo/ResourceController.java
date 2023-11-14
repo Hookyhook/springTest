bcef4e2f6a7b7f28875e0d254dd80a49f7af05bb
Hookyhook <justus.von.voss@gmail.com>
2023-11-14 21:21:57 +0100

    Added jpa support

--- src/main/java/com/example/demo/ResourceController.java
+++ src/main/java/com/example/demo/ResourceController.java
@@ -3,18 +3,24 @@ package com.example.demo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/student")
 public class ResourceController {
 
     @Autowired
     StudentRepository studentRepository;
 
     @GetMapping
-    public Optional<Student> getStudent(@RequestParam(name = "id") int id){
-        return studentRepository.findById(id);
+    public List<Student> getStudent(@RequestParam(name = "id", required = false) Integer id){
+        if(id != null){
+            return List.of(studentRepository.findById(id).orElse(null));
+        }
+        return  studentRepository.findAll();
     }
+
 
     @PostMapping
     public Student postStudent(@RequestBody Student student){
